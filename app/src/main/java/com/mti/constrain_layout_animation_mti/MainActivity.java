e2b2715efbe5d5a8fe835c4178afc5bbca584190
Tareq3 <mti.tareq3@gmail.com>
2018-05-28 14:28:12 +0600

    branch_02_initial_commit

--- app/src/main/java/com/mti/constrain_layout_animation_mti/MainActivity.java
+++ app/src/main/java/com/mti/constrain_layout_animation_mti/MainActivity.java
@@ -8,29 +8,50 @@ import android.transition.TransitionInflater;
 import android.transition.TransitionManager;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.ImageView;
+import android.widget.Toast;
 
 public class MainActivity extends AppCompatActivity {
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.scene1);
 
-        findViewById(R.id.close_Button).setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View v) {
-                if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.KITKAT) {
-                    TransitionManager.go(
-                            Scene.getSceneForLayout(
-                                    (ViewGroup)findViewById(R.id.root),
-                                    R.layout.scene2,
-                                    MainActivity.this
 
-                            ),
-                            TransitionInflater.from(MainActivity.this).inflateTransition(R.transition.defaul_to_info)
-                    );
-                }
-            }
-        });
+
+
+
+
+    }
+
+    public void start_Scene2(View view){
+        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.KITKAT) {
+            TransitionManager.go(
+                    Scene.getSceneForLayout(
+                            (ViewGroup)findViewById(R.id.root),
+                            R.layout.scene2,
+                            MainActivity.this
+
+                    ),
+                    TransitionInflater.from(MainActivity.this).inflateTransition(R.transition.defaul_to_info)
+            );
+        }
+    }
+
+    public void start_Scene1(View view) {
+      //  Toast.makeText(MainActivity.this, "clicked", Toast.LENGTH_SHORT).show();
+        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.KITKAT) {
+            TransitionManager.go(
+                    Scene.getSceneForLayout(
+                            (ViewGroup) findViewById(R.id.root),
+                            R.layout.scene1,
+                            MainActivity.this
+
+                    ),
+                    TransitionInflater.from(MainActivity.this).inflateTransition(R.transition.back_to_default)
+            );
+        }
     }
 }
