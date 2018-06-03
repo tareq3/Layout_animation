bcdffbf112aa8d321974ec59702e983cca827ca1
Tareq3 <mti.tareq3@gmail.com>
2018-06-04 02:37:58 +0600

    branch_1.x.C_initial_commit

--- app/src/main/java/com/mti/constrain_layout_animation_mti/MainActivity.java
+++ app/src/main/java/com/mti/constrain_layout_animation_mti/MainActivity.java
@@ -27,15 +27,23 @@ public class MainActivity extends AppCompatActivity {
     private static boolean show;
     ViewGroup sceneRoot;
 
+    /* ConstraintSet class allows you to define programmatically
+     a set of constraints(means element's properties and coordination like position, color, text) to be used with ConstraintLayout.
+     It lets you create and save constraints, and apply them to an existing ConstraintLayout. so no need to change layout
+     *** just change layout elements properties
+    */
+    ConstraintSet constraintSet ;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main_scene_0);
 
+
          sceneRoot=findViewById(R.id.constraint);
+        constraintSet=new ConstraintSet();
 
-
+        //as Here layout not changing is not changing so we can call the click listener directly
          findViewById(R.id.backgroundImage).setOnClickListener(new View.OnClickListener() {
              @Override
              public void onClick(View v) {
@@ -54,30 +62,26 @@ public class MainActivity extends AppCompatActivity {
     private  void showComponents() {
         show = true;
 
-        ConstraintSet constraintSet = new ConstraintSet();
-        constraintSet.clone((Context)this, R.layout.activity_main_scene_1); //secondery layout
+
+        constraintSet.clone((Context)this, R.layout.activity_main_scene_1); //cloning constrainSet means copying the elements properties and coordination
 
         ChangeBounds transition = new ChangeBounds();
         transition.setInterpolator((TimeInterpolator)(new AnticipateOvershootInterpolator(1.0F)));
         transition.setDuration(1200L);
 
         TransitionManager.beginDelayedTransition(sceneRoot,transition);
-        constraintSet.applyTo((ConstraintLayout) sceneRoot);
+        constraintSet.applyTo((ConstraintLayout) sceneRoot); //apply all constraint element properties from scene_1 to sceneroot , nothing is replacing just changing the coordination
     }
 
     //In this following method we are using transition xml which is doing the same thing like previous method
     private void hideComponents() {
         show = false;
 
-        ConstraintSet constraintSet = new ConstraintSet();
+
         constraintSet.clone((Context)this, R.layout.activity_main_scene_0);
-        ChangeBounds transition = new ChangeBounds();
-
-        transition.setInterpolator((TimeInterpolator)(new AnticipateOvershootInterpolator(1.0F)));
-        transition.setDuration(1200L);
 
 
-        TransitionManager.beginDelayedTransition(sceneRoot,transition);
+        TransitionManager.beginDelayedTransition(sceneRoot,TransitionInflater.from(this).inflateTransition(R.transition.simple_transition));
         constraintSet.applyTo((ConstraintLayout) sceneRoot);
     }
 }
